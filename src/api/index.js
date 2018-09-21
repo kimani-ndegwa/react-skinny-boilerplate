dcd71b64c90f252d6acce2fc43c2b9276a31312e
kimani-ndegwa <kimani@mavenmachines.com>
2018-09-21 21:54:26 +0300

    (API) Results loaded from API

---
+++ src/api/index.js
@@ -0,0 +1,6 @@
+/**
+ * API Calls
+ */
+
+export const BASE_URL =
+  "https://apis.mavenmachines.com/directoryService/employee?employee_id=7&version=2";
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,88 @@
-export const App = () => "Coming to you skinny but live!"
+import React from "react";
+
+import {BASE_URL} from "../api";
+
+export class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      URLValue: "",
+      resultsLoaded: false,
+      results: {}
+    };
+
+    this.onChangeURLInput = this.onChangeURLInput.bind(this);
+  }
+
+  /**
+   * Life Cycle
+   */
+
+  componentWillMount() {
+    console.log("here")
+    fetch(BASE_URL,
+       {
+      mode: "cors", 
+      cache: "no-cache", 
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+    }
+    
+    )
+      .then(response => {
+        if(response.ok){
+            response.json().then(r=>{
+                console.log(r, "Response Here");
+                this.setState({
+                    results: r
+                })
+            })
+        } else {
+            throw new Error(response);
+        }
+      })
+      .catch(err => {
+        console.log(err, "ERROR IS HERE");
+      });
+  }
+
+  onChangeURLInput(event) {
+    const URLValue = event.target.value;
+    this.setState({
+      URLValue
+    });
+  }
+  render() {
+    const { URLValue } = this.state;
+    
+    return (
+      <div className="container">
+        <div className="url-section">
+          <label>Enter URL:</label>
+          <input
+            type="text"
+            onChange={this.onChangeURLInput}
+            placeholder={"Enter URL"}
+            value={URLValue}
+          />
+        </div>
+
+        <div className="method-section">
+          <h2>Methods</h2>
+          <div className="methods">
+            <div className="method">GET</div>
+            <div className="method">POST</div>
+          </div>
+        </div>
+
+        {/* <div className="params-section">
+          <div className="method">params</div>
+          <div className="method">body</div>
+        </div> */}
+      </div>
+    );
+  }
+}
